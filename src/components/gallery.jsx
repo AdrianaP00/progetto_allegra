71e6902f40eb7a6ab8f636a70e37d523e7d18126
AdrianaP00 <adrianapiccolo00@gmail.com>
2024-08-28 20:13:23 +0200

    add fonts , and images to gallery, change text

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -6,13 +6,23 @@ export const Gallery = (props) => {
     <div id="portfolio" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Galería</h2>
+          <h2>Instantes gastronómicos</h2>
+          <p></p>
+          <p></p>
           <p>
-            Selecciono personalmente cada uno de los ingredientes utilizados en la
+            Mi deseo principal de Progetto Allegra es crear instantes gastronómicos
+            personalizados. Organizo pequeños y exclusivos eventos, cenas, cócteles,
+            comidas privados y de negocios en vuestras residencias o en el lugar que os
+            apetezca.
+            Mi savoir- faire me permite junto a mi equipo de sala, ofrecerles una experiencia
+            única , en la que cada plato tiene una historia que revindica el placer de
+            compartir y en la que cada decoración de mesa pone el escenario. Preparo para
+            el deleite de mis comensales una cocina
+            creativa con raíces sicilianas, suma y fusión de sabores, cocciones y aromas del
+            mediterráneo, Selecciono personalmente cada uno de los ingredientes utilizados en la
             preparación de mis recetas y puedo decir con satisfacción que he desarrollado
             una relación única con pequeños productores y proveedores basada en el
-            respeto y la confianza. Ellos comparten conmigo la pasión por el trabajo bien
-            hecho y los productos orgánicos y de óptima calidad.
+            respeto y la confianza.
           </p>
         </div>
         <div className="row">
@@ -29,6 +39,7 @@ export const Gallery = (props) => {
                     smallImage={d.smallImage}
                   />
                 </div>
+
               ))
               : "Loading..."}
           </div>
