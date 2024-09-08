9f59667cbee58b10fb7fad37c17eb9751d9f8ade
AdrianaP00 <adrianapiccolo00@gmail.com>
2024-09-08 02:40:24 +0200

    add new dependency

---
+++ build/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "WEB3FORMS_ACCESS_KEY": "eccb7982-ebce-49b3-b22f-17b588f13c95"
+};
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -3,29 +3,31 @@ import React from "react";
 export const Services = (props) => {
   return (
     <div id="radio" className="text-center">
-        <div className="container">
-          <div className="col-md-5 col-md-offset-2 section-title">
-            <h2>Entrevistas</h2>
-            <p>Escuchame aquí</p>
-          </div>
-          <div id="row" className="row">
-            {props.data
-              ? props.data.map((d, i) => (
-                  <div key={`${d.name}-${i}`} className="col-md-6 col-sm-6 radio">
-                    <div>
-                      <a href={d.link}>
-                        <img src={d.img} alt="..." className="radio-img" />
-                      </a>
-                      <div className="caption">
-                        <h4>{d.name}</h4>
-                        <p>{d.job}</p>
-                      </div>
-                    </div>
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Entrevistas</h2>
+          <p>Escuchame aquí</p>
+        </div>
+        <div id="row" className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+              <div>    <div key={`${d.name}-${i}`} className="col-md-6 col-sm-6 radio">
+                <div>
+
+                  <a href={d.link}>
+                    <img src={d.img} alt="..." className="radio-img" />
+                  </a>
+
+                  <div className="caption">
+                    <h4>{d.name}</h4>
+                    <p>{d.job}</p>
                   </div>
-                ))
-              : "loading"}
-          </div>
+                </div>
+              </div></div>
+            ))
+            : "loading"}
         </div>
       </div>
+    </div>
   );
 };
