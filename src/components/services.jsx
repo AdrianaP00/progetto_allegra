e0dbb1fc9531639d631a7fcb9b01ee5c0e563714
AdrianaP00 <adrianapiccolo00@gmail.com>
2024-09-08 02:24:12 +0200

    update radio section style

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -3,32 +3,29 @@ import React from "react";
 export const Services = (props) => {
   return (
     <div id="radio" className="text-center">
-      <div className="container">
-        <div className="col-md-8 col-md-offset-2 section-title">
-          <h2>Entrevistas</h2>
-          <p>
-          Escuchame aquí
-          </p>
-        </div>
-        <div id="row">
-        {props.data
-            ? props.data.map((d, i) => (
-              <div key={`${d.name}-${i}`} className="col-md-8 col-sm-8 radio">
-                <div className="thumbnail">
-                  {" "}
-                  <a href={d.link}>
-                    <img src={d.img} alt="..." className="radio-img" />
-                  </a>
-                  <div className="caption">
-                    <h4>{d.name}</h4>
-                    <p>{d.job}</p>
+        <div className="container">
+          <div className="col-md-5 col-md-offset-2 section-title">
+            <h2>Entrevistas</h2>
+            <p>Escuchame aquí</p>
+          </div>
+          <div id="row" className="row">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div key={`${d.name}-${i}`} className="col-md-6 col-sm-6 radio">
+                    <div>
+                      <a href={d.link}>
+                        <img src={d.img} alt="..." className="radio-img" />
+                      </a>
+                      <div className="caption">
+                        <h4>{d.name}</h4>
+                        <p>{d.job}</p>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            ))
-            : "loading"}
+                ))
+              : "loading"}
+          </div>
         </div>
       </div>
-    </div>
   );
 };
